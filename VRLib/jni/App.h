e69dfae016e2fb622d2473bc4d3db9c7d9e40cf1
wenyule <1101534564@qq.com>
2015-04-09 14:19:42 +0800

    添加中文测试

--- VRLib/jni/App.h
+++ VRLib/jni/App.h
@@ -1,7 +1,7 @@
 /************************************************************************************
 
 Filename    :   App.h
-Content     :   Native counterpart to VrActivity
+Content     :   Native counterpart to VrActivity±æª˙”ÎVrActivity
 Created     :   September 30, 2013
 Authors     :   John Carmack
 
